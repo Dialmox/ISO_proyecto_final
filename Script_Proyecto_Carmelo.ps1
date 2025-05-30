49daf208e030f126bc15764c6cb5f167fc3de3e1
Victux11 <vmnavasg@gmail.com>
2025-05-30 13:30:44 +0200

    Add files via upload

---
+++ Script_Proyecto_Carmelo.ps1
@@ -0,0 +1,9 @@
+﻿# Script para mostrar el uso de disco en Windows
+
+Write-Host "Espacio en disco por unidad:`n"
+Get-PSDrive -PSProvider 'FileSystem' | ForEach-Object {
+    $totalGB = "{0:N2}" -f ($_.Used + $_.Free)/1GB
+    $usadoGB = "{0:N2}" -f $_.Used/1GB
+    $libreGB = "{0:N2}" -f $_.Free/1GB
+    Write-Host "$($_.Name): Usado: $usadoGB GB | Libre: $libreGB GB | Total: $totalGB GB"
+}
